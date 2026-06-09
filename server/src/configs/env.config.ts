42cc53ed816e8063327d10a46873f165f620c1fa
Jay <jayrathore88155@gmail.com>
2026-06-09 19:23:53 +0530

    Add env.configs for env variables

---
+++ server/src/configs/env.config.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({path : `.env.${process.env.NODE_ENV || 'development'}.local`});
+
+export const {
+  MONGODB_URI , 
+  COOKIE_SECRET ,   
+  SALT_ROUND , 
+  JWT_SECRET,  
+  FRONTEND , 
+} = process.env;
