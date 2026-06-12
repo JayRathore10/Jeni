590b7e7d056e187276f6299c6cc5b2e303d08407
Jay <jayrathore88155@gmail.com>
2026-06-12 12:09:26 +0530

    Add user Model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,1 +1,54 @@
-// Add model and connect them 
+import { Schema, model, Document, Types } from "mongoose";
+
+export interface IUser extends Document {
+  _id: Types.ObjectId;
+
+  name: string;
+  email: string;
+  password: string;
+
+  avatar?: string;
+
+  totalStorageUsed: number;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    avatar: {
+      type: String,
+      default: "",
+    },
+
+    totalStorageUsed: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = model<IUser>("User", UserSchema);
