2545c8afdef07c8ed19395d44ddacdfd5e8c3a0f
Jay <jayrathore88155@gmail.com>
2026-06-19 20:47:17 +0530

    Add folder router to the app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,8 @@
 import express  , {Request , Response} from "express";
-import { multerRouter } from "./routes/multer.routes";
 import authRouter from "./routes/auth.routes";
 import { FRONTEND } from "./configs/env.config";
 import userRouter from "./routes/user.routes";
+import folderRouter from "./routes/folder.routes";
 import cors from "cors";
 
 const app = express();
@@ -19,8 +19,8 @@ app.get("/"  , (req : Request, res : Response)=>{
   res.send("Hi, Jexts here!")
 })
 
-app.use("/multer", multerRouter);
 app.use("/api/v1/auth" , authRouter);
 app.use("/api/v1/users" , userRouter);
+app.use("/api/v1/folder" , folderRouter);
 
 export default app;
