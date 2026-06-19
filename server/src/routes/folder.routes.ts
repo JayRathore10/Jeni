5fb10d7c74a96a6980b736317bc5982be7ee4504
Jay <jayrathore88155@gmail.com>
2026-06-19 20:45:42 +0530

    add all routes to folder routes

--- server/src/routes/folder.routes.ts
+++ server/src/routes/folder.routes.ts
@@ -1,5 +1,45 @@
-import {Router} from "express";
+import { Router } from "express";
+import { authMiddleware } from "../middleware/auth.middleware";
+
+import {
+  createFolder,
+  getFolders,
+  getFolderById,
+  updateFolder,
+  deleteFolder,
+  getFolderContents,
+} from "../controllers/folder.controller";
 
 const router = Router();
+
+/**
+ * CREATE FOLDER
+ */
+router.post("/", authMiddleware, createFolder);
+
+/**
+ * GET ALL USER FOLDERS
+ */
+router.get("/", authMiddleware, getFolders);
+
+/**
+ * GET SINGLE FOLDER
+ */
+router.get("/:folderId", authMiddleware, getFolderById);
+
+/**
+ * GET FOLDER CONTENTS (FILES + SUBFOLDERS)
+ */
+router.get("/:folderId/contents", authMiddleware, getFolderContents);
+
+/**
+ * UPDATE FOLDER (rename / move)
+ */
+router.patch("/:folderId", authMiddleware, updateFolder);
+
+/**
+ * DELETE FOLDER
+ */
+router.delete("/:folderId", authMiddleware, deleteFolder);
 
 export default router;
