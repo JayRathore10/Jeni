f4c97e3d64432aa6a4f8f78644226ed702ad5058
Jay <jayrathore88155@gmail.com>
2026-06-24 18:43:50 +0530

    Add downloaadLog routes

--- server/src/routes/downloadLog.routes.ts
+++ server/src/routes/downloadLog.routes.ts
@@ -1,6 +1,16 @@
-import {Router} from "express";
+import { Router } from "express";
+import {
+  createDownloadLog,
+  getAllDownloadLogs,
+  getDownloadLogsByShareLink,
+  deleteDownloadLog,
+} from "../controllers/downloadLog.controller";
 
 const router = Router();
 
+router.post("/", createDownloadLog);
+router.get("/", getAllDownloadLogs);
+router.get("/share/:shareLinkId", getDownloadLogsByShareLink);
+router.delete("/:id", deleteDownloadLog);
 
 export default router;
