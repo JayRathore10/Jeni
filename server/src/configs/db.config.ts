518c9c67e2175783290dde187e29073aef8bb427
Jay <jayrathore88155@gmail.com>
2026-06-30 15:04:09 +0530

    Add the DB name to it

--- server/src/configs/db.config.ts
+++ server/src/configs/db.config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
   try{ 
-    await mongoose.connect("mongodb://localhost:27017/myDB");
+    await mongoose.connect("mongodb://localhost:27017/jeni");
     console.log("DB Connected Successfully");
   }catch(err){
     console.log(err);
